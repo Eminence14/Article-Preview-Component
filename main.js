a939535f9e424c7526a6551a261af33b62a20fe1
Victor <victoribidunmoye@gmail.com>
2022-09-06 15:44:08 +0100

    project completed with few bugs

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const footer1 = document.querySelector('.foot')
 const footer2 = document.querySelector('.foot_replace')
+const footDesktop = document.querySelector('.foot_replace3')
 const share = document.querySelector('#share')
 const icons = document.querySelectorAll('.icons')
+const svgArrow = document.querySelector('#arrow')
 
 footer2.style.display = 'none'
+footDesktop.style.display = 'none'
+
+if (window.innerWidth < 1200) {
+    svgArrow.style.display = 'none'
+}
 
 share.addEventListener('click', showIcons)
 for (let icon of icons) {
@@ -14,6 +21,10 @@ function showIcons() {
     if (window.innerWidth < 1200) {
         footer1.style.display = 'none'
         footer2.style.display = 'flex'
+    }
+    if (window.innerWidth > 1200) {
+        footer2.style.display = 'none'
+        footDesktop.style.display = 'flex'
     }
 }
 
